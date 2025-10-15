a62d34900991134f6dce5d0599fb605f855d7df2
Mhmd-abi-hachem <mohamadhachem908@gmail.com>
2025-10-15 23:30:34 +0300

    add missing padding to login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,7 +4,7 @@ import Logo from "../shared/ui/Logo";
 
 function Login() {
   return (
-    <main className="min-h-screen grid md:grid-cols-[48rem] bg-[color:var(--color-grey-50)] place-content-center gap-8 bg-grey-50 !overflow-hidden">
+    <main className="min-h-screen grid md:grid-cols-[48rem] bg-[color:var(--color-grey-50)] place-content-center !px-5 md:!px-0 gap-8 bg-grey-50 !overflow-hidden">
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
